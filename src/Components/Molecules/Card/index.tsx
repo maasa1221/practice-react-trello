ee575f4bdf729af94c60fcc9acd44fa9fc2a06f1
渡辺　昌 <maasa1221@gmail.com>
2020-04-06 22:36:04 +0900

    rename

--- src/Components/Molecules/Card/index.tsx
+++ src/Components/Molecules/Card/index.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import styles from "./style";
 // import { ButtonType } from "../../../Constants/Button";
-import { Button, ButtonProps } from "../../Atoms/Button/index"
-import { Text, TextProps } from "../../Atoms/Text"
+import { Button, ButtonProps } from "../../Atoms/Button/index";
+import { Text, TextProps } from "../../Atoms/Text";
 // import { Input, InputProps } from "../../Atoms/Input"
 
 const { StyledDiv, StyledGrid1, StyledGrid2 } = styles;
 
-export interface CardProps {
+export interface ListProps {
   textProps: TextProps;
   buttonProps: ButtonProps;
-
 }
 
-export const Card: React.FC<CardProps> = ({
-  buttonProps, textProps
-}) => (
-
-    <StyledDiv>
-      <StyledGrid1><Text {...textProps} /></StyledGrid1>
-      <StyledGrid2><Button {...buttonProps} /></StyledGrid2>
-    </StyledDiv>
-  );
-
+export const List: React.FC<ListProps> = ({ buttonProps, textProps }) => (
+  <StyledDiv>
+    <StyledGrid1>
+      <Text {...textProps} />
+    </StyledGrid1>
+    <StyledGrid2>
+      <Button {...buttonProps} />
+    </StyledGrid2>
+  </StyledDiv>
+);
--- src/pages/table/Table.tsx
+++ src/pages/table/Table.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Card, CardProps } from "../../Components/Molecules/Card"
-import { RouteComponentProps } from 'react-router-dom';
-import { ActionDispatcher } from './Container';
+import React from "react";
+import { List, ListProps } from "../../Components/Molecules/Card";
+import { RouteComponentProps } from "react-router-dom";
+import { ActionDispatcher } from "./Container";
 
 interface Props extends RouteComponentProps<{}> {
-    actions: ActionDispatcher
+  actions: ActionDispatcher;
 }
 
-
-export const Table: React.FC<Props> = props => {
-    const cardProps: CardProps = {
-        textProps: { label: "Doing" },
-        buttonProps: { label: "+ さらにカードを追加", buttonType: 1, width: "10rem", fontsize: "0.6rem" }
-    }
-    return (
-        <>
-            <Card {...cardProps} />
-            <Card {...cardProps} />
-            {/* 配列を受け取って、内部にカードを複製する */}
-        </>
-    );
-}
+export const Table: React.FC<Props> = (props) => {
+  const listProps: ListProps = {
+    textProps: { label: "Doing" },
+    buttonProps: {
+      label: "+ さらにカードを追加",
+      buttonType: 1,
+      width: "10rem",
+      fontsize: "0.6rem",
+    },
+  };
+  return (
+    <>
+      <List {...listProps} />
+      <List {...listProps} />
+      {/* 配列を受け取って、内部にカードを複製する */}
+    </>
+  );
+};
